2910884b1b3b2a0d64b26f481a54e05c9cf53662
LAB202 <LAB202@LAB206-PC05>
2019-09-11 09:41:18 -0500

    primer proyecto

---
+++ primerproyecto/src/primerproyecto/Primerproyecto.java
@@ -0,0 +1,14 @@
+
+package primerproyecto;
+
+
+public class Primerproyecto {
+
+  
+    public static void main(String[] args) {
+      
+    System.out.println("primer proyecto");
+        
+    }
+    
+}
